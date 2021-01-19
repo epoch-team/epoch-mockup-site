4705373a94c184494e8d7a55b07cf8a8530c9712
y-kurata <yoshi-kurata@ncontr.com>
2021-01-19 13:32:03 +0900

    update

--- src/ita_api_test_prj/settings.py
+++ src/ita_api_test_prj/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/dist/'
+STATIC_URL = '/mockup/'
 STATICFILES_DIRS = (
     ['/workspace/src/templates']
 )
